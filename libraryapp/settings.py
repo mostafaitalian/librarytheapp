6fbca2014bb470bf5cff1c414cda27de4a56d8cd
mostafaitalian <eng_mustafa_yossef@hotmail.com>
2021-07-04 01:06:17 +0200

    ghghghghg

--- libraryapp/settings.py
+++ libraryapp/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["librarytheapp.herokuapp.com", '0.0.0.0']
 
 
 # Application definition
