caad5fb2b42068a1fb29ab51d6599b32363302b7
mostafaitalian <eng_mustafa_yossef@hotmail.com>
2021-07-04 00:51:14 +0200

    ghghgh

--- libraryapp/settings.py
+++ libraryapp/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -138,3 +138,5 @@ try:
     from .local_settings import *
 except ImportError:
     raise Exception('local settings file is required')
+
+django_heroku.settings(locals())
